5891f04e6c7170c152a5f6fee6deb59a3328a546
HafeezIlias <HafeezIlias.1@gmail.com>
2025-11-10 15:07:52 +0800

    update scripts

--- WorkshopThings/cypress.config.setup.js
+++ WorkshopThings/cypress.config.setup.js
@@ -15,7 +15,7 @@ export default defineConfig({
       // return config for Cypress
       return config;
     },
-    baseUrl: "https://example.cypress.io",
+    baseUrl: "https://example.cypress.io", 
   },
 });
 
@@ -24,15 +24,38 @@ export default defineConfig({
 import "allure-cypress";
 
 // THIS SCRIPT WE WILL BE ADDING TO THE PACKAGE.JSON FILE TO EASE OUR USAGE OF ALLURE WITH CYPRESS
-"scripts": {
-  "cypress:run": "npx cypress run",
-  "allure:generate": "npx allure generate allure-results --clean -o allure-report",
-  "allure:open": "npx allure open allure-report",
-  "test:allure": "npm run cypress:run && npm run allure:generate && npm run allure:open"
-}
+  "scripts": {
+    "start": "node server/server.js",
+    "dev": "node server/server.js",
+    "cypress:open": "cypress open",
+    "cypress:run": "cypress run",
+    "clean": "node -e \"const fs=require('fs'); ['allure-results','allure-report','cypress/screenshots','cypress/videos'].forEach(d=>{try{fs.rmSync(d,{recursive:true,force:true})}catch(e){}})\"",
+    "test:login": "npm run clean && cypress run --spec \"cypress/e2e/01-login.cy.js\" --browser chrome && npx allure generate allure-results --clean -o allure-report && npx allure open allure-report"
+  },
 
 // Setting auto record video
     video: true,
 // Setting auto screenshot
     screenshotOnRunFailure: true,
+
+
+
+////////////////////// CUSTOM METHOD FOR CYPRESS TO BE ADD IN THE COMMAND.JS ///////////////////////
+
+/**
+ * Wait for toast notification
+ * @param {string} message - Expected toast message
+ * @param {string} type - Toast type (success, error, warning, info)
+ */
+Cypress.Commands.add('waitForToast', (message, type = null) => {
+  if (type) {
+    cy.get(`.toast.${type}`).should('be.visible');
+  } else {
+    cy.get('.toast').should('be.visible');
+  }
+
+  if (message) {
+    cy.get('.toast-message').should('contain', message);
+  }
+});
 // END OF WORKSHOP CYPRESS AND ALLURE BY HAFEEZ
